e3b395d0685b18d4f5262c418dfa4682d432a4e8
Yurtkan Yurtoğulları <12103132+yurtkan@users.noreply.github.com>
2023-06-19 06:19:43 +0300

    Final

--- api/inc/config.php
+++ api/inc/config.php
@@ -1,5 +1,5 @@
 <?php
     define("DB_HOST", "localhost");
-    define("DB_USERNAME", "ecampusdbuser");
-    define("DB_PASSWORD", "IRDzwsBu0X(yFhOR");
-    define("DB_DATABASE_NAME", "ecampus");
+    define("DB_USERNAME", "user");
+    define("DB_PASSWORD", "pass");
+    define("DB_DATABASE_NAME", "db");
